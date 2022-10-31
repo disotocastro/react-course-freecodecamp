edc991bba0eb790d8af1de042b261a9db3eed33a
Diego Soto <juandisoca@gmail.com>
2022-10-31 14:09:18 -0600

    ADD Jornal Card component and  data.js

--- my-travel-journal/src/Components/Cards.jsx
+++ my-travel-journal/src/Components/Cards.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
+import img from '../Assets/placeholder.png'
+import location from '../Assets/location.png'
 
 const Cards = () => {
   return (
     <div className='card--container'>
-      <img src={''} alt="" />
+      <img src={img} alt="" />
 
       <div className="card--info">
         <div className="card-location">
-          <img src={''} alt="" />
-          <h3 className="card--location">Nombre</h3>
+          <img className='location-img' src={location} alt="" />
+          <h3 className="card--location">Pais</h3>
           <a className='card--link' href="http://google.com" target="_blank" rel="noopener noreferrer">View on Google Maps</a>
         </div>
 
-        <h1 className='card--name'>Name</h1>
-        <h4 className='card--date'>Date</h4>
-        <p className='card--description'>Blablabla</p>
+        <h1 className='card--name'>Mount Fuji</h1>
+        <h4 className='card--date'>12 Jan, 2021 - 24 Jan, 2021</h4>
+        <p className='card--description'>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
 
       </div>
     </div>
---
+++ my-travel-journal/src/data.js
@@ -0,0 +1,41 @@
+const data = [
+  {
+    title: "CDMX",
+    location: "México",
+    googleMapsUrl: "https://goo.gl/maps/LLa9UnV3emuRrTZz6",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description: "La Ciudad de México (CDMX), antes el Distrito Federal (D. F.), es la capital de los Estados Unidos Mexicanos. Es una de las entidades federativas que, junto con treinta y un estados, conforman dicho país. Asimismo, es sede de los Poderes de la Unión. Está dividida en dieciséis demarcaciones territoriales.",
+    image: "mexico-cdmx.png"
+  },
+  {
+    title: "Orlando, FL",
+    location: "Estados Unidos",
+    googleMapsUrl: "https://goo.gl/maps/xxCfpQT9vhTQLL2LA",
+    startDate: "12 Jan, 2020",
+    endDate: "24 Jan, 2020",
+    description: "La Ciudad de México (CDMX), antes el Distrito Federal (D. F.), es la capital de los Estados Unidos Mexicanos. Es una de las entidades federativas que, junto con treinta y un estados, conforman dicho país. Asimismo, es sede de los Poderes de la Unión. Está dividida en dieciséis demarcaciones territoriales.",
+    image: "eeuu-orlando.png"
+  },
+  {
+    title: "Paso Canoas",
+    location: "Panamá",
+    googleMapsUrl: "https://goo.gl/maps/TRyDq7MsK8huSx987",
+    startDate: "20 Nov, 2016",
+    endDate: "23 Nov, 2016",
+    description: "La Ciudad de México (CDMX), antes el Distrito Federal (D. F.), es la capital de los Estados Unidos Mexicanos. Es una de las entidades federativas que, junto con treinta y un estados, conforman dicho país. Asimismo, es sede de los Poderes de la Unión. Está dividida en dieciséis demarcaciones territoriales.",
+    image: "panama-paso-canoas.png"
+  }
+
+
+
+
+
+
+
+
+
+
+]
+
+export default data
