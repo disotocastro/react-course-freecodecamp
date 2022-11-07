6fd3cea7ee9a59ea52a15055610042a1dc1add66
Diego Soto <juandisoca@gmail.com>
2022-11-07 15:01:27 -0600

    Add Meme -> setMeme state

--- meme-generator/src/Components/Inputs.jsx
+++ meme-generator/src/Components/Inputs.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import memesData from '../memesData';
 const Inputs = () => {
 
+  const [allMemeImages, setAllMemeImages] = useState(memesData)
+
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    botText: "",
+    randomImg: "https://i.imgflip.com/30b1gx.jpg",
+  })
+
+
   let url
-
-
-  const [memeImage, setMemeImage] = React.useState("")
-
+  const [memeImage, setMemeImage] = React.useState()
 
   function getMemeImage() {
-    const memesArray = memesData.data.memes
+    const memesArray = allMemeImages.data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length)
     url = memesArray[randomNumber].url
-    setMemeImage(url)
+    setMeme(prevState => ({
+      ...prevState,
+      randomImg: url
+    }))
   }
+
+
+
+
+
+
+
 
   return (
     <div className='form-container'>
@@ -23,7 +39,7 @@ const Inputs = () => {
         <input className='form__input' type="text" placeholder='Bottom Text' />
       </div>
       <button onClick={getMemeImage} className='form__btn'>Get a new meme image</button>
-      <img src={memeImage} alt="asdasd" />
+      <img src={meme.randomImg} className="meme__image" alt="asdasd" />
     </div>
   );
 };
