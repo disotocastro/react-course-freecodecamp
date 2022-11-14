2cea80d7c81ac53c0aed09d1819bd3e74b13040e
Diego Soto <juandisoca@gmail.com>
2022-11-14 13:54:38 -0600

    handle change agregado

--- meme-generator/src/Components/Inputs.jsx
+++ meme-generator/src/Components/Inputs.jsx
@@ -10,6 +10,15 @@ const Inputs = () => {
     randomImg: "https://i.imgflip.com/30b1gx.jpg",
   })
 
+  function handleChange(event) {
+    const { name, value } = event.target
+    setMeme(prevMeme => {
+      return {
+        ...prevMeme,
+        [name]: value
+      }
+    })
+  }
 
   let url
   const [memeImage, setMemeImage] = React.useState()
@@ -26,20 +35,50 @@ const Inputs = () => {
 
 
 
-
-
-
-
-
   return (
     <div className='form-container'>
       <p>{url}</p>
       <div className='form' action="">
-        <input className='form__input' type="text" placeholder='Top text' />
-        <input className='form__input' type="text" placeholder='Bottom Text' />
+        <input
+          className='form__input'
+          type="text"
+          placeholder='Top text'
+          onChange={handleChange}
+          name={"topText"}
+          value={FormData.topText}
+
+        />
+        <input
+          className='form__input'
+          type="text"
+          placeholder='Bottom Text'
+          onChange={handleChange}
+          name={"botText"}
+          value={FormData.botText}
+
+        />
       </div>
       <button onClick={getMemeImage} className='form__btn'>Get a new meme image</button>
-      <img src={meme.randomImg} className="meme__image" alt="asdasd" />
+
+      <div className="meme">
+        <img src={meme.randomImg} className="meme__image" alt="asdasd" />
+        <h2 className='meme--text top'>{meme.topText}</h2>
+        <h2 className='meme--text bottom'> {meme.botText}</h2>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   );
 };
