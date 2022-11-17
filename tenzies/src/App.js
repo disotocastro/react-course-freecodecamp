697e13e52768601c40da715cf81d12060aa9cd0a
Diego Soto <juandisoca@gmail.com>
2022-11-17 15:44:12 -0600

    ADD: Tenzies -> New Background color

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -7,14 +7,14 @@ const App = () => {
         <div className="tenzies--background">
           <div className="die--container">
             <Die value={1} />
+            <Die value={5} />
+            <Die value={6} />
+            <Die value={3} />
+            <Die value={4} />
             <Die value={1} />
-            <Die value={1} />
-            <Die value={1} />
-            <Die value={1} />
-            <Die value={1} />
-            <Die value={1} />
-            <Die value={1} />
-            <Die value={1} />
+            <Die value={5} />
+            <Die value={2} />
+            <Die value={3} />
             <Die value={1} />
           </div>
         </div>
