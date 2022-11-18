fcb9ea5c0c96d395913f935c42f69d8ec80d0eb9
Diego Soto <juandisoca@gmail.com>
2022-11-18 08:54:36 -0600

    ADD: Tenzies -> Random dice generated

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,39 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Die from './Die'
 
 
 
-
-function allNewDice() {
-  let arrayDice = []
-  for (let i = 0; i < 10; i++) {
-    arrayDice[i] = Math.floor(Math.random() * 6) + 1;
-
-  }
-  console.log(arrayDice);
-}
-
-
-allNewDice()
-
-
-
 const App = () => {
+
+  const [dice, setDice] = useState(allNewDice())
+
+  const diceElements = dice.map(die => <Die value={die} />)
+
+
+
+  function allNewDice() {
+    let arrayDice = []
+    for (let i = 0; i < 10; i++) {
+      arrayDice.push(Math.floor(Math.random() * 6) + 1);
+
+    }
+    return arrayDice
+  }
+
+
+
   return (
     <div>
       <main className="tenzies--background-container">
         <div className="tenzies--background">
           <div className="die--container">
-            <Die value={1} />
-            <Die value={5} />
-            <Die value={6} />
-            <Die value={3} />
-            <Die value={4} />
-            <Die value={1} />
-            <Die value={5} />
-            <Die value={2} />
-            <Die value={3} />
-            <Die value={1} />
+            {diceElements}
           </div>
         </div>
       </main>
