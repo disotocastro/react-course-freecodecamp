059194033c2ecfff94b77b592b17580dfaaa8456
Diego Soto <juandisoca@gmail.com>
2022-11-21 00:17:24 -0600

    ADD: Tenzies -> rollDice new function added, instructions added

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -8,20 +8,26 @@ const App = () => {
 
   const [dice, setDice] = useState(allNewDice())
 
-
-  function rollDice() {
-    setDice(allNewDice())
+  function generateNewDie() {
+    return {
+      value: (Math.floor(Math.random() * 6) + 1),
+      isHeld: false,
+      id: nanoid()
+    }
   }
 
+  function rollDice() {
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld ?
+        die :
+        generateNewDie()
+    }))
+  }
 
   function allNewDice() {
     let arrayDice = []
     for (let i = 0; i < 10; i++) {
-      arrayDice.push({
-        value: (Math.floor(Math.random() * 6) + 1),
-        isHeld: false,
-        id: nanoid()
-      });
+      arrayDice.push(generateNewDie());
 
     }
     return arrayDice
@@ -36,11 +42,12 @@ const App = () => {
         die
     }))
   }
-
   return (
     <div>
       <main className="tenzies--background-container">
         <div className="tenzies--background">
+          <h1 className="title">Tenzies</h1>
+          <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
           <div className="die--container">
             {diceElements}
           </div>
